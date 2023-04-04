45a5f6e0a649513282b548cdae2f7fe8a3e23170
Gavin Uhran <gavinu2001@gmail.com>
2023-04-04 11:15:39 -0400

    Update

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import './LogIn.css';
 
 function LogIn() {
     return (
-        <div className="side-info">Log In</div>
+        <div className="side-info">
+            <div className="logo-header">
+                <div className="logo-icon"></div>
+                <h3 className="logo-title">CookShare</h3>
+          </div>
+          <h1 className="side-text">Discover the world's top<br />Chefs & Bakers.</h1>
+          <div className="illustration"></div>
+        </div>
     );
 }
 
